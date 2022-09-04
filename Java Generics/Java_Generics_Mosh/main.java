d679f20b0554252fa3356c4c7be9b6186ffd6d3f
kfahad <77542699+kokfahad@users.noreply.github.com>
2022-09-04 19:40:59 +0600

    generics mosh code added

---
+++ Java Generics/Java_Generics_Mosh/main.java
@@ -0,0 +1,14 @@
+public class main {
+    public static void main(String[] args) {
+//        var list = new List();
+//        list.add(1);
+//        list.add("1");
+//        list.add(new User());
+        var list = new GenericList<>();
+        list.add(1);
+        list.add(1.6d);
+//        list.add("1");
+//        list.add(new User());
+//        Integer item = list.get(0);
+    }
+}
